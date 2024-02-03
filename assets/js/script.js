342439a160842720103209f5e2a8e87e2281e032
Luiza Kormann <luiza.kormann@gmail.com>
2024-02-03 18:22:10 -0300

    logo e pg sobre

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+/* Acessando elemento por ID --     MAIS USADOS --*/
+let nome = window.document.getElementById("nome")
+
+/* Acessando elemento por tag */ 
+let mensagem = document.getElementsByTagName("textarea")
+
+/* Acessando elemento por nome */
+
+/* Acessando elemento por classe */
+
+/* Acessando elemento por seletor -- MAIS USADOS --*/ 
+let email = document.querySelector("#email")
+
+nome.style.width = "100%"
+email.style.width = "100%"
